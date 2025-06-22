d63e88b0e074c98000d7d44b5962ef2e7c538e9f
Victor Bascope <jusn.victor.bascope.castro@gmail.com>
2025-06-22 14:19:27 -0400

    configuracion para gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { ghPages } from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
   optimizeDeps: {
@@ -10,7 +11,8 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [vue()],
+  plugins: [vue(), ghPages()],
+  base: "portafolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
