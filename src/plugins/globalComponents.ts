3a25bfd7cc0cdb12c0aea74aef31c68aec23923d
Victor Bascope <jusn.victor.bascope.castro@gmail.com>
2025-09-06 21:23:47 -0400

    componente loading como componente global

--- src/plugins/globalComponents.ts
+++ src/plugins/globalComponents.ts
@@ -1,8 +1,10 @@
+import Loading from "@/shared/components/Loading.vue";
 import Modal from "@/shared/components/Modal.vue";
 import type { App } from "vue";
 
 export default {
   install(app: App) {
     app.component("Modal", Modal);
+    app.component("Loading", Loading);
   },
 };
